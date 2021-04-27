a2017e762f681c63069ea877856a10622dbd4d2c
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-04-26 22:54:28 -0400

    Added OpenWeatherMapServiceTest

--- src/main/java/benedek/openweathermap/OpenWeatherMapService.java
+++ src/main/java/benedek/openweathermap/OpenWeatherMapService.java
@@ -7,5 +7,5 @@ import retrofit2.http.Query;
 public interface OpenWeatherMapService
 {
     @GET("/data/2.5/weather?appid=492f7559e977abb236b0e006a71cdf33")
-    Single<OpenWeatherMapService>getCurrentWeather(@Query("q") String location);
+    Single<OpenWeatherMapFeed> getCurrentWeather(@Query("q") String location);
 }
