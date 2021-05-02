c56a840f74ec3fac99e3e4903a73ed997cf5a70e
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-05-02 14:57:38 -0400

    Added OpenWeatherMapApplication, Controller and fxml

---
+++ src/main/java/benedek/openweathermap/OpenWeatherMapApplication.java
@@ -0,0 +1,26 @@
+package benedek.openweathermap;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class OpenWeatherMapApplication extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/weather_application.fxml"));
+
+        Scene scene = new Scene(root, 500, 275);
+
+        stage.setTitle("Weather");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/benedek/openweathermap/OpenWeatherMapController.java
@@ -0,0 +1,5 @@
+package benedek.openweathermap;
+
+public class OpenWeatherMapController
+{
+}
